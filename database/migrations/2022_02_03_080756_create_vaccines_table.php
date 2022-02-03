327e1893296cac9ac566a1fabbe65d0e4778be2a
minariee <98794838+minariee@users.noreply.github.com>
2022-02-03 16:18:53 +0800

    added vaccines table

---
+++ database/migrations/2022_02_03_080756_create_vaccines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVaccinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vaccines', function (Blueprint $table) {
+            $table->id();
+            $table->string('vaccine_type', 80);
+            $table->string('vaccine_brand', 100);
+            $table->integer('current_dose');
+            $table->date('latest_dosage_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vaccines');
+    }
+}
