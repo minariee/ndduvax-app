f9b420a05a600697ec841e2ae952f61e27545053
jamltg <98795730+jamltg@users.noreply.github.com>
2022-02-04 21:41:09 +0800

    loginpage already made (view)

--- app/Http/Controllers/LoginPageController.php
+++ app/Http/Controllers/LoginPageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TermsAndConditionController extends Controller
+class LoginPageController extends Controller
 {
     public function index() {
         return view('loginscreen', [
--- resources/views/loginscreen.blade.php
+++ resources/views/loginscreen.blade.php
@@ -8,35 +8,40 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>NDDU-VAX Login</title>
   </head>
   <body>
-        <section class="Form">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-5">
-                        <img src="https://en.wikipedia.org/wiki/Notre_Dame_of_Dadiangas_University#/media/File:NDDUseal.png" class="img-fluid" alt="">
-                    </div>
-                    <div class="col-lg-7">
-                        <form>
-                            <div class="form-row">
-                              <div class="col-lg-7">
-                                <input type="email" placeholder="Email Address" class="form-control">
-                             </div>
-                            </div>
-                            <div class="form-row">
-                              <div class="col-lg-7">
-                                <input type="password" placeholder="******" class="form-control">
-                             </div>
-                            </div>
-                            <div class="form-row">
-                              <div class="col-lg-7">
-                                <button type="button" class="btn1">Login</button>
-                             </div>
-                            </div>
-                        </form>
-                    </div>
-        </section>            
+    
+    <div class="text-center mt-5">
+      <form style="max-width:380px;margin:auto;">     
+
+      <form>
+        <img class="mt-4 mb-4" src="https://upload.wikimedia.org/wikipedia/en/d/d9/NDDUseal.png" class="png" height="120" alt="NDDU Logo">
+        <h1 class="h3 mb-3 font-weight-normal">NDDU-VAX SIGN IN</h1>
+        <label for= "emailAddress"
+        class="sr-only">Email Address</label>
+        <input type="email" id="emailAddress"
+        class="form-control" placeholder="Email Address" required autofocus>
+
+        <label for="password"Password
+        class="sr-only">Password</label>
+        <input type="password" id="password"
+        placeholder="Password" class="form-control">
+
+        <div class="mt-3">
+          <button class="btn btn-lg btn-primary btn-block">Log In</button>
+        </div>
+
+      </form>
+
+    </form>
+
+    </div>
+   
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
