a35dfa3cde3014742913aa9e33ebfcbd69a1eb56
minariee <98794838+minariee@users.noreply.github.com>
2022-02-06 23:33:34 +0800

    home updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -90,11 +90,11 @@
           <strong class="d-inline-block mb-2 text-primary">World</strong>
           <h3 class="mb-0">Featured post</h3>
           <div class="mb-1 text-muted">8 December 2020</div>
-          <p class="card-text mb-auto">Germs are all around us, both in our environment and in our bodies. When a person is susceptible and they encounter a harmful organism, it can lead to disease and death.</p>
+          <p class="card-text mb-auto">This article is part of a series of explainers on vaccine development and distribution. Learn more about vaccines – from how they work and how they’re made to ensuring safety and equitable access.</p>
           <a href="https://www.who.int/news-room/feature-stories/detail/how-do-vaccines-work?adgroupsurvey={adgroupsurvey}&gclid=Cj0KCQiAgP6PBhDmARIsAPWMq6mxOrvw13jAwitFNKx6Qdx5Eij3BHw_TeS-1oKz9OYfymzFbJnwgx0aAr7JEALw_wcB" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="https://www.who.int/images/default-source/vaccines-explained/vaccines-antibody-illustration-02-29-oct.jpg?Status=Master&sfvrsn=a79ee886_36" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
         </div>
       </div>
@@ -103,10 +103,10 @@
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
           <strong class="d-inline-block mb-2 text-success">National</strong>
-          <h3 class="mb-0">Post title</h3>
-          <div class="mb-1 text-muted">Nov 11</div>
-          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
+          <h3 class="mb-0">Updates on COVID-19 Vaccines</h3>
+          <div class="mb-1 text-muted">24 February 2022</div>
+          <p class="mb-auto">Let’s realize a COVID-protected Philippines! As more Filipinos are vaccinated, we are a few more steps closer to brighter days in the new normal.</p>
+          <a href="https://doh.gov.ph/vaccines" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
           <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
@@ -119,28 +119,28 @@
   <div class="row g-5">
     <div class="col-md-8">
       <h3 class="pb-4 mb-4 fst-italic border-bottom">
-        From the Firehose
+        News
       </h3>
 
       <article class="blog-post">
-        <h2 class="blog-post-title">Sample blog post</h2>
-        <p class="blog-post-meta">January 1, 2021 by <a href="#">Mark</a></p>
+        <h2 class="blog-post-title">Philippines reaches 54M fully vaccinated persons almost 2 weeks into 2022</h2>
+        <p class="blog-post-meta">Published January 14, 2022 10:13pm by <a href="#">RICHA NORIEGA, GMA News</a></p>
 
-        <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.</p>
+        <p>More than 54 million of the Philippine population have been fully vaccinated against COVID-19 amid the sudden steep increase in cases which started after Christmas, the National Task Force Against COVID-19 said on Friday.</p>
         <hr>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h2>Blockquotes</h2>
-        <p>This is an example blockquote in action:</p>
+        <p>Data from the National Vaccination Operations Center (NVOC) as of January 13 showed that 54,457,863 individuals have been fully vaccinated or 70.60% of the government's target for vaccination.</p>
+        <h2>The Philippines, thus, reached less than a week into 2022 the milestone vaccine czar Secretary Carlito Galvez said the government wanted to reach by the end of 2021.</h2>
         <blockquote class="blockquote">
-          <p>Quoted text goes here.</p>
+          <p>Galvez said in a statement.
+        </p>
         </blockquote>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h3>Example lists</h3>
-        <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout. This is an example unordered list:</p>
+        <p>“Malaki ang naging epekto ng Bagyong Odette sa ating pagbabakuna kaya bahagyang naantala ang pag-abot sa target nating 54 million fully vaccinated individuals sa pagtatapos ng 2021,"</p>
+        <p>"Sinabayan pa ito ng panibagong surge ngayong Enero, pero hindi tayo tumigil sa pagbabakuna,” he added.</p>
+        <h3>Other Stories</h3>
         <ul>
-          <li>First list item</li>
-          <li>Second list item with a longer description</li>
-          <li>Third list item to close it out</li>
+          <li><a href="https://www.gmanetwork.com/news/topstories/nation/818220/doh-coming-up-with-system-to-include-antigen-self-test-results-in-covid-19-tally/story/">DOH coming up with system to include antigen self-test results in COVID-19 tally</a></li>
+          <li><a href="https://www.gmanetwork.com/news/topstories/nation/818207/unvaccinated-persons-form-long-queues-crowd-vax-sites/story/">Unvaccinated persons form long queues, crowd vax sites</a></li>
+          <li><a href="https://www.gmanetwork.com/news/topstories/nation/818170/gov-t-quarantine-facilities-must-provide-isolation-rooms/story/">Gov't: Quarantine facilities must provide isolation rooms</a></li>
         </ul>
         <p>And this is an ordered list:</p>
         <ol>
@@ -247,34 +247,21 @@
     <div class="col-md-4">
       <div class="position-sticky" style="top: 2rem;">
         <div class="p-4 mb-3 bg-light rounded">
-          <h4 class="fst-italic">About</h4>
-          <p class="mb-0">Customize this section to tell your visitors a little bit about your publication, writers, content, or something else entirely. Totally up to you.</p>
+          <h4 class="fst-italic">About Us</h4>
+          <p class="mb-0">This Website offers you vaccine tracker that enable you to see updates and news regarding vaccines. </p>
         </div>
 
-        <div class="p-4">
-          <h4 class="fst-italic">Archives</h4>
-          <ol class="list-unstyled mb-0">
-            <li><a href="#">March 2021</a></li>
-            <li><a href="#">February 2021</a></li>
-            <li><a href="#">January 2021</a></li>
-            <li><a href="#">December 2020</a></li>
-            <li><a href="#">November 2020</a></li>
-            <li><a href="#">October 2020</a></li>
-            <li><a href="#">September 2020</a></li>
-            <li><a href="#">August 2020</a></li>
-            <li><a href="#">July 2020</a></li>
-            <li><a href="#">June 2020</a></li>
-            <li><a href="#">May 2020</a></li>
-            <li><a href="#">April 2020</a></li>
-          </ol>
-        </div>
+            <div class="p-4 mb-3 bg-light rounded">
+                <h4 class="fst-italic"> <a href = "https://vaxcert.doh.gov.ph/">Get Your Vax Cert</a></h4>
+                <p class="mb-0"> <a href = "https://vaxcert.doh.gov.ph/">  <img src="assets/images/m6.png" class="css-class"></a></p>
+            </div>
 
         <div class="p-4">
           <h4 class="fst-italic">Elsewhere</h4>
           <ol class="list-unstyled">
-            <li><a href="#">GitHub</a></li>
+            <li><a href="https://www.nddu.edu.ph/">NDDU Website</a></li>
             <li><a href="#">Twitter</a></li>
-            <li><a href="#">Facebook</a></li>
+            <li><a href="https://www.facebook.com/NDDUofficial/">Facebook</a></li>
           </ol>
         </div>
       </div>
