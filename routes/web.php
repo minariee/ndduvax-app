8dfa45cf6c5a00eb9d6cf331c1ce850cb0d90042
jamltg <98795730+jamltg@users.noreply.github.com>
2022-02-06 21:21:32 +0800

    viewregistrationedit
    also added route for userdashboard (web.php)

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,8 @@ Route::get('/vaccine-form', '\App\Http\Controllers\VaccineFormController@index')
 Route::post('/vaccine-form', '\App\Http\Controllers\VaccineFormController@submit');
 Route::get('/terms-and-condition', '\App\Http\Controllers\TermsAndConditionController@index');
 Route::get('/home-page', '\App\Http\Controllers\HomePageController@index');
-Route::get('/user-dashboard', '\App\Http\Controllers\UserDashboardController@index');
 Route::get('/login-page', '\App\Http\Controllers\LoginPageController@index');
-Route::post('/login-page', '\App\Http\Controllers\LoginController@submit');
 Route::get('/registration-page', '\App\Http\Controllers\RegistrationController@index');
+Route::get('/user-dashboard', '\App\Http\Controllers\UserDashboardController@index');
 
 
