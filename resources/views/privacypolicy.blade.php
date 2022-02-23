71c64b7c7c8edf28f0d9bb617169cad8a6222902
jamltg <98795730+jamltg@users.noreply.github.com>
2022-02-23 18:59:11 +0800

    privacy-policy

--- resources/views/privacypolicy.blade.php
+++ resources/views/privacypolicy.blade.php
@@ -15,17 +15,61 @@
 
         }
 
+
     </style>
 
 <body>
     <div id="container">
 
-        <h1 style = "font-family:arial;font-size:27px;font-style:bold">Privacy Policy</h1>
+        <h1 style = "font-family:arial;font-size:40px;font-style:bold"><b>INTRODUCTION</b></h1>
 
-        <div id="content">
-            <p style= "font-family:arial">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam numquam aspernatur reiciendis autem ea magnam accusamus doloremque. Est facere repudiandae sit recusandae, totam deleniti maiores ullam pariatur doloribus, delectus itaque.</p>
-        </div>
+        <p>Welcome to the Notre Dame of Dadiangas University, Inc. This Privacy Policy is about our policy in terms of collecting, using, or processing your personal data, in regards to your engagement with the University.</p>
+        <p>We respect your right to privacy and strive to abide by all applicable privacy and data protection regulations, including the Data Privacy Act of 2012 (DPA). We, like the National Privacy Commission (NPC), strive to strike a balance between your personal privacy and public safety. Regarding the free flow of information, particularly while pursuing our legitimate interests and fulfilling our obligations as an educational institution, employer, and/or contracting partner.</p>
+        <p>The terms “data” and “information” are used interchangeably throughout this Policy. When we talk about “personal data,” we’re talking about personal information, sensitive personal information, and privileged data.</p>
+        <p style= "margin-bottom:60px"><i>(Please take time to read the following policies below)</i></p>
+    
+        <h2 style = "font-family:arial;font-size:28px;font-style:bold;margin-bottom:30px">PRIVACY POLICY FOR UNIVERSITY PERSONNEL</h2>
+           
+        <p><b>WHAT</b> – We gather your personal information in a variety of ways. Written documents, picture, and digital content may be included. Here are several examples:</p>
+        
+        <ol>
+            <li style="margin-bottom:20px">Information we gather or produce throughout your employment or involvement with the University. We may collect additional information about you after you join the University, we may also get other types of data from you especially health information which will be recorded and handled by the school nurse.</li>
+            <li style="margin-bottom:30px">Information that has been given to you without your permission. Your personal information may be transferred to or received by us without our permission. In such situations, we’ll assess whether we have the legal authority to keep the information. Otherwise, if you provide us with the personal data of others (for example, a person to call in an emergency, professional referees), we will ask you to confirm that you have acquired their consent before giving us their information.</li>
+        </ol>
 
+        <p><b>WHY</b> – We use your data to pursue our legitimate interests as an educational institution, employer, and/or contracting party, including a range of administrative, research, historical, and statistical purposes, to the degree authorized or required by law.</p>
+        <p>We believe that processing your data for these reasons is essential for us to fulfill our contractual commitments to you, to comply with a legal duty, to safeguard your critically important interests, such as your life and health, and to carry out public-interest responsibilities (e.g., public order, public safety, etc.), or in the pursuit of the University’s or a third party’s legitimate interest. We recognize that the DPA sets more stringent regulations for the handling of sensitive personal information and privileged information, and we are completely committed to following them.</p>
+        <p>If we need your permission for a specific use of your data, we will ask for it when the time comes.</p>
+        <p style="margin-bottom:50px">Please be aware that we will not use your data for any automated decision-making without your permission.</p>
+        
+        <p style="margin-bottom:60px"><b>HOW</b> – Your personal information is securely kept and sent in a variety of paper and electronic formats, including databases, shared across the University’s many departments or offices. Access to your data is restricted to university employees who have a legitimate interest in them in order to fulfill their contractual obligations. Rest assured that our use of your data will be limited to what is necessary to achieve the purpose for which they were collected, and/or only when permitted by law.</p>
+    
+        <p><b>For more information you may contact the following:</b></p>
+        <p>Questions: info@nddu.edu.ph</p>
+        <p style="margin-bottom:40px">Complaints / Security Incidents:  mis@nddu.edu.ph.</p>
+        <p>Landline: (083) 552-4444</p>
+        <p style="margin-bottom:60px">Address: HR 115, Notre Dame of Dadiangas University, Marist Avenue, General Santos City 9500</p>
+        
+        <h3 style = "font-family:arial;font-size:28px;font-style:bold;margin-bottom:30px">PRIVACY POLICY FOR STUDENTS</h3>
+        <p><b>WHAT</b> – We gather your personal information in a variety of ways. Written documents, picture, and digital content may be included. Here are several examples:</p>
+    
+        <ol>
+            <li style="margin-bottom:20px">Information we gather or produce throughout your employment or involvement with the University. We may collect additional information about you after you join the University, we may also get other types of data from you especially health information which will be recorded and handled by the school nurse.</li>
+            <li style="margin-bottom:30px">Information that has been given to you without your permission. Your personal information may be transferred to or received by us without our permission. In such situations, we’ll assess whether we have the legal authority to keep the information. Otherwise, if you provide us with the personal data of others (for example, a person to call in an emergency, professional referees), we will ask you to confirm that you have acquired their consent before giving us their information.</li>
+        </ol>
+
+        <p><b>WHY</b> – We use your data to further our legitimate interests as an educational institution, including a range of academic, administrative, research, historical, and statistical purposes, to the degree authorized or required by law.</p>
+        <p>We believe that processing your data for these reasons is essential for us to fulfill our contractual commitments to you, to comply with a legal duty, to safeguard your critically important interests, such as your life and health, and to carry out public-interest responsibilities (e.g., public order, public safety, etc.), or in the pursuit of the University’s or a third party’s legitimate interest. We recognize that the DPA sets more stringent regulations for the handling of sensitive personal information and privileged information, and we are completely committed to following them.</p>
+        <p>If we need your permission for a specific use of your data, we will ask for it when the time comes.</p>
+        <p style="margin-bottom:50px">Please be aware that we will not use your data for any automated decision-making without your permission.</p>
+    
+        <p><b>HOW</b> – Personal information is securely kept and sent in a variety of paper and electronic media, including databases shared across the University’s many departments or offices. Access to your data is restricted to university employees who have a legitimate interest in knowing it in order to fulfill their contractual obligations. Rest assured that we will not misuse your personal information.</p>
+        <p style="margin-bottom:60px">We shall keep your relevant personal data for historical and statistical reasons forever unless otherwise required by law or acceptable University regulations. If a retention term is mandated by law or University policy, all impacted records will be safely destroyed at the end of that time.</p>
+        <p><b>For more information you may contact the following:</b></p>
+        <p>Questions: info@nddu.edu.ph</p>
+        <p style="margin-bottom:40px">Complaints / Security Incidents:  mis@nddu.edu.ph.</p>
+        <p>Landline: (083) 552-4444</p>
+        <p style="margin-bottom:60px">Address: HR 115, Notre Dame of Dadiangas University, Marist Avenue, General Santos City 9500</p>
     </div>
 </body>
 </html>
