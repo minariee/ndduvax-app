9cb3678117cbe304eff86944d879f79483a05531
minariee <98794838+minariee@users.noreply.github.com>
2022-04-14 21:09:50 +0800

    SMS send to all users

--- app/Http/Controllers/SemaphoreController.php
+++ app/Http/Controllers/SemaphoreController.php
@@ -9,8 +9,8 @@ use App\Services\SMS;
 class SemaphoreController extends Controller
 {
     public function index() {
-        $users =User::select('mobile_number')->get();
-        
+        //$users =User::select('mobile_number')->get();
+        $users =User::all('mobile_number');
         foreach ($users as $user) {
             $user->mobile_number;    
         }
@@ -21,32 +21,34 @@ class SemaphoreController extends Controller
         ]);
     }
     public function send(User $users,Request $request){
-        $users =User::select('mobile_number')->get();
-        
+        $users =User::all('mobile_number');
         foreach ($users as $user) {
-            $user->mobile_number;    
+           
+            $user->mobile_number;
+            $ch = curl_init();
+            $mobiles = $user->mobile_number;  
+            $message = $request->input('message');
+            
+            $parameters = array(
+                'apikey' => env('SEMAPHORE_KEY'), //Your API KEY
+                'number' => $mobiles,
+                'message' => $message,
+                'sendername' => 'SEMAPHORE'
+            );
+
+            curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/messages' );
+            curl_setopt( $ch, CURLOPT_POST, 1 );
+    
+            //Send the parameters set above with the request
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
+    
+            // Receive response from server
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+            $output = curl_exec( $ch );
+            curl_close ($ch);
         }
-        $message = $request->input('message');
-        $mobiles = $user->mobile_number;
-        $ch = curl_init();
-        $parameters = array(
-            'apikey' => env('SEMAPHORE_KEY'), //Your API KEY
-            'number' => $mobiles,
-            'message' => $message,
-            'sendername' => 'SEMAPHORE'
-        );
-        curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/messages' );
-        curl_setopt( $ch, CURLOPT_POST, 1 );
-
-        //Send the parameters set above with the request
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
-
-        // Receive response from server
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        $output = curl_exec( $ch );
-        curl_close ($ch);
-
+      
         //Show the server response
-    return view('smsemaphore')->with('successMsg','Message sent successfully .');;
+        return view('smsemaphore')->with('successMsg','Message sent successfully .');
     }    
 }
