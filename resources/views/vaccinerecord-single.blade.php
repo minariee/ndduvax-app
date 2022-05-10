0d9bf93d1ebddc58f45d1f7fb92d5ee5053dbfe9
minariee <98794838+minariee@users.noreply.github.com>
2022-05-10 18:08:54 +0800

    modal

--- resources/views/vaccinerecord-single.blade.php
+++ resources/views/vaccinerecord-single.blade.php
@@ -121,6 +121,30 @@
               data-toggle="modal" data-target="#form-modal"
               >Add Record</button>
               @endrole
+              
+              <button type="button" class="btn btn-success"   data-toggle="modal" data-target="#exampleModal">
+              Add Record
+              </button>
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Send Proof of Vaccination</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <p>Send Vaccine Proof to: <strong>vaccinetrackernddu@gmail.com</strong> </p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
             </div>
             <div class="card-body pt-0">
               <table class="table" id="vaccination-table">
@@ -209,4 +233,8 @@
     })
     
   </script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 @endsection
